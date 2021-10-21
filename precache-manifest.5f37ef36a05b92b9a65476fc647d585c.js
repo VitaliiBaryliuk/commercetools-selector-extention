20aa9a87a8b730d93bda6232b7955e2925954a79
Vitalii Baryliuk <vitalii_baryliuk@epam.com>
2021-10-22 00:02:38 +0300

    Add build files

---
+++ precache-manifest.5f37ef36a05b92b9a65476fc647d585c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b231f9a2bd05bb18cb0c00a8cec677",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f221157628507c4ba146",
+    "url": "./static/css/main.b501dcee.chunk.css"
+  },
+  {
+    "revision": "de7af642a03c8e89cd77",
+    "url": "./static/js/2.0bf45033.chunk.js"
+  },
+  {
+    "revision": "d51efca734e49bd703ca68088f877ed2",
+    "url": "./static/js/2.0bf45033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f221157628507c4ba146",
+    "url": "./static/js/main.155b8979.chunk.js"
+  },
+  {
+    "revision": "7f4d444b9055ebe4039c",
+    "url": "./static/js/runtime-main.87ee3bb8.js"
+  }
+]);
